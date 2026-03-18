3ab26900ca38091ed67649552fd626890d111dce
Alex Tempel <alex.dirk.tempel@gmail.com>
2026-03-18 13:37:23 -0400

    Add UDP Classes

---
+++ bcm/UDPClient.cpp
@@ -0,0 +1,68 @@
+#include "UDPClient.hpp"
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cstdint>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+
+UDPClient::UDPClient(const std::string& ipAddr, uint16_t portNumber) : m_portNumber(portNumber) {
+    m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (m_socket < 0) {
+        std::cerr << "Could not open socket!" << std::endl;
+        throw std::runtime_error("Couldn't create socket");
+    }
+
+    // 0 initialize struct
+    std::memset(&m_serverAddress, 0, sizeof(m_serverAddress));
+    m_serverAddress.sin_family = AF_INET;
+    m_serverAddress.sin_port = htons(m_portNumber);
+
+    if (inet_pton(AF_INET, ipAddr.c_str(), &m_serverAddress.sin_addr) <= 0) {
+        close(m_socket);
+        throw std::runtime_error("Invalid IP Address");
+    }
+}
+
+UDPClient::~UDPClient() {
+    if (m_socket >= 0) {
+        close(m_socket);
+    }
+}
+
+bool UDPClient::sendData(std::vector<char>& data) {
+    ssize_t sent = sendto(
+        m_socket, 
+        data.data(), 
+        data.size(), 
+        0, 
+        reinterpret_cast<struct sockaddr*>(&m_serverAddress), 
+        sizeof(m_serverAddress)
+    );
+
+    return sent == data.size();
+}
+
+std::vector<char> UDPClient::receiveData(size_t length) {
+    std::vector<char> data;
+    data.resize(length);
+    
+    socklen_t addressLength = sizeof(m_serverAddress);
+
+    recvfrom(
+        m_socket, 
+        data.data(), 
+        length, 
+        0, 
+        reinterpret_cast<struct sockaddr*>(&m_serverAddress), 
+        &addressLength
+    );
+
+    return data;
+}
---
+++ bcm/UDPServer.cpp
@@ -0,0 +1,63 @@
+#include "UDPServer.hpp"
+#include <cstring>
+#include <netinet/in.h>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+UDPServer::UDPServer(uint16_t portNumber) : m_portNumber(portNumber) {
+    m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (m_socket < 0) {
+        throw std::runtime_error("Failed to create socket");
+    }
+
+    std::memset(&m_serverAddress, 0, sizeof(m_serverAddress));
+    m_serverAddress.sin_family = AF_INET;
+    m_serverAddress.sin_addr.s_addr = INADDR_ANY;
+    m_serverAddress.sin_port = htons(m_portNumber);
+
+    if (bind(m_socket, reinterpret_cast<struct sockaddr*>(&m_serverAddress), sizeof(m_serverAddress)) < 0) {
+        close(m_socket);
+        throw std::runtime_error("Bind failed");
+    }
+}
+
+UDPServer::~UDPServer() {
+    if (m_socket >= 0) {
+        close(m_socket);
+    }
+}
+
+std::vector<char> UDPServer::receiveData(size_t length, sockaddr_in& clientAddress) {
+    std::vector<char> data;
+    data.resize(length);
+
+    socklen_t addressLength = sizeof(clientAddress);
+
+    recvfrom(
+        m_socket, 
+        data.data(), 
+        data.size(), 
+        0, 
+        reinterpret_cast<struct sockaddr*>(&clientAddress), 
+        &addressLength
+    );
+
+    return data;
+}
+
+bool UDPServer::sendData(std::vector<char> data, const sockaddr_in& clientAddress) {
+    ssize_t sent = sendto(
+        m_socket, 
+        data.data(), 
+        data.size(), 
+        0, 
+        reinterpret_cast<const struct sockaddr*>(&clientAddress), 
+        sizeof(clientAddress)
+    );
+
+    return sent == data.size();
+}
